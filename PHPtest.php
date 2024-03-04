f57714a11c771a0c6f29bf5ff3d3b1abc2f41f4d
Aakash_dubey999 <84366886+Akwarrior222@users.noreply.github.com>
2024-03-04 11:26:03 +0530

    Add files via upload

---
+++ PHPtest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Simple Webpage</title>
+</head>
+<body>
+
+<h1>Welcome to My Simple Webpage</h1>
+
+<?php
+// PHP code to display the current date and time
+echo "Current date and time: " . date("Y-m-d H:i:s");
+?>
+
+<p>This is a paragraph in my webpage.</p>
+
+</body>
+</html>
